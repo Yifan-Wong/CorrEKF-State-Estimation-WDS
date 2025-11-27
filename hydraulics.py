e970103a02a21b75fd7538ffd9d7ff4f8f1359ef
Yifan-Wong <ivanwong199808@gmail.com>
2025-11-26 22:04:24 -0600

    Remove scattered files from root

--- hydraulics.py
+++
@@ -1,9 +0,0 @@
-try:
-    import numba
-    _HAS_NUMBA = True
-except:
-    _HAS_NUMBA = False
-if _HAS_NUMBA:
-    from pipedream_solver.nsuperlink import nSuperLink as SuperLink
-else:
-    from pipedream_solver.superlink import SuperLink as SuperLink
--- transport.py
+++
@@ -1,9 +0,0 @@
-try:
-    import numba
-    _HAS_NUMBA = True
-except:
-    _HAS_NUMBA = False
-if _HAS_NUMBA:
-    from pipedream_solver.nquality import QualityBuilder
-else:
-    raise ImportError('Requires `numba` library.')
